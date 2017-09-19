c7b21301f903eff2b73d33b0251275e4693faf20
John Chleborad <jchleborad@gmail.com>
2017-09-18 19:48:02 -0700

    app.js comments

--- app.js
+++ app.js
@@ -23,15 +23,20 @@
 // }
 
 // TODO LIST
+//Set up the controler and an empty array, todoList
   var app = angular.module('myApp', []);
   app.controller('todoCtrl', function($scope) {
     $scope.todoList = [];
 
+//Create a function to push input into the todoList Array, set initial value to unchecked (done:false)
+//Initial state of input field is empty (= "")
     $scope.todoAdd = function() {
       $scope.todoList.push({todoText:$scope.todoInput, done:false});
       $scope.todoInput = "";
     };
 
+//Create a copy of todoList, call it "oldList" and set todoList array to empty
+//Loop through oldList an for each instance where item is not done, push item back into todoList 
     $scope.remove = function() {
       var oldList = $scope.todoList;
       $scope.todoList = [];
